0264804787522841b8e97786b0a73f7ff3d472ec
김규나 <kkn00077@gmail.com>
2023-12-17 14:57:03 +0900

    요청사항 수정 + 폰트어썸 추가

--- components/LogoBox.tsx
+++ components/LogoBox.tsx
@@ -5,13 +5,23 @@ import { fadeIn, mainColorBorderBottom } from "@/style";
 import { motion } from "framer-motion";
 import Image from "next/image";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFileLines, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
 export const LogoBox = () => {
     return (
         <motion.div
             className={`bg-white py-1 flex flex-col justify-center items-center border-b-1 border-b-[#1c8549]`}
             {...fadeIn}
         >
-            <Image width={155} height={25} className="opacity-0" src={Logo} alt="DB손해보험 로고" />
+            {/* <Image width={155} height={25} className="opacity-0" src={Logo} alt="DB손해보험 로고" /> */}
+            
+            <div className="h-11 flex flex-row justify-center items-center">
+                <FontAwesomeIcon className="px-1" icon={faMagnifyingGlass} rotation={90} size="2xl" />
+                <FontAwesomeIcon className="px-1" icon={faFileLines} size="xl" />
+            </div>
+            
+
         </motion.div>
     );
 };
